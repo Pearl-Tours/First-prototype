59babf642cb1ea6d682ea3b9c4b738722b98d593
GEORGE MUTALE <mutalegeorge367@gmail.com>
2025-06-27 10:19:47 +0200

    db

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ load_dotenv()
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.models import Base
-#SQLALCHEMY_DATABASE_URL = "mysql+pymysql://admin:1237tyu@db:3306/tourdb"
+# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://admin:1237tyu@db:3306/tourdb"
 #SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  # Use SQLite for simplicity
 engine = create_engine(SQLALCHEMY_DATABASE_URL )
